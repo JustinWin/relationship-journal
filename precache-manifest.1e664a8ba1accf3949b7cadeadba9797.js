3934b8f08601b140e4e89ea34d323266bdb64a07
Justin Nguyen <justin454500@gmail.com>
2022-06-26 23:29:16 -0400

    Updates

---
+++ precache-manifest.1e664a8ba1accf3949b7cadeadba9797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab4cdfec1a85f4bc2de6",
+    "url": "/relationship-journal-project/static/css/main.c6ff95ea.chunk.css"
+  },
+  {
+    "revision": "ab4cdfec1a85f4bc2de6",
+    "url": "/relationship-journal-project/static/js/main.dc6cef9a.chunk.js"
+  },
+  {
+    "revision": "e3b3160f65166d41fc47",
+    "url": "/relationship-journal-project/static/js/runtime~main.af23b66b.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/relationship-journal-project/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/relationship-journal-project/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/relationship-journal-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "50bd5a48196ed1d3813a6be35f4dcb3d",
+    "url": "/relationship-journal-project/index.html"
+  }
+];
